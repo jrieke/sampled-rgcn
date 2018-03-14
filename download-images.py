726fe069105b1113b882806be92104e07f9fb0f1
Johannes Rieke <johannes.rieke@gmail.com>
2018-03-14 15:21:50 +0100

    Custom re-implementation, added relational version

--- download-images.py
+++ download-images.py
@@ -1,27 +1,41 @@
-# Download all images from the URL lists in parallel.
+"""
+Download and preprocess images for the ImageGraph dataset.
+
+Before running this script, please download the URL lists from
+https://github.com/nle-ml/mmkb and unpack to data/fb15k-237/image-graph_urls.
+Uses multiprocessing to process the images in parallel. Skips images that
+were already downloaded.
+
+Usage:
+    python download-images.py [google|bing|yahoo]
+"""
+
 
 from __future__ import absolute_import, division, print_function, unicode_literals
-
 import pandas as pd
-
 import urllib
 import urlparse
-from PIL import Image
+from PIL import Image, ImageFile
 from tqdm import tqdm
 import os
 import errno
 from ssl import CertificateError
 from httplib import HTTPException
 from multiprocessing import Pool
+import sys
 
 
-provider = 'google'
+ImageFile.LOAD_TRUNCATED_IMAGES = True  # prevent PIL from raising an error if a file is truncated
+
+if len(sys.argv) > 1:
+    provider = sys.argv[1]
+else:
+    provider = 'google'
+
 urls = pd.read_csv('data/fb15k-237/image-graph_urls/URLS_{}.txt'.format(provider), sep='\t', names=['url', 'id'])
 
 
 def download_image(i):
-
-    #global counter_fail, counter_success, counter_skip
 
     row = urls.iloc[i]
     url = row['url']
@@ -42,16 +56,16 @@ def download_image(i):
 
         # Skip images that were already downloaded.
         if os.path.exists(os.path.join(target_dir, target_filename)):
-            #counter_skip += 1
             pass
         else:
             try:
                 # Download and open image.
+                # TODO: Store temporary files in a separate folder (e.g. image-graph_temp),
+                #       so they are not hidden in the system folders.
+                temp_filename = ''
                 temp_filename, _ = urllib.urlretrieve(row['url'])
                 im = Image.open(temp_filename)
             except (IOError, CertificateError, HTTPException):
-                #print('Failed:', row['url'])
-                #counter_fail += 1
                 pass
             except Exception as e:
                 print('Got unusual error during downloading/opening of image:', e)
@@ -63,17 +77,16 @@ def download_image(i):
 
                 # Save.
                 im.save(os.path.join(target_dir, target_filename))
-                #counter_success += 1
+            finally:
+                # Remove temporary file.
+                try:
+                    os.remove(temp_filename)
+                except OSError:
+                    pass
 
-
-#counter_fail = 0
-#counter_success = 0
-#counter_skip = 0
 
 pool = Pool(100)  # use many processes here because some of them will get stuck waiting for server responses
 
 with tqdm(total=len(urls)) as pbar:
     for _ in pool.imap_unordered(download_image, urls.index, 100):
         pbar.update()
-
-#print('Successfully downloaded', counter_success, 'images, failed on', counter_fail, 'images, skipped', counter_skip, 'images')
