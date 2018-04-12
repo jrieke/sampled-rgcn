5aec07c9616ace19f49a75c87890f8cb91f4c994
Johannes Rieke <johannes.rieke@gmail.com>
2018-04-12 12:09:10 +0200

    Added YAGO, more sophisticated training runs, before refactoring into modules

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ from tqdm import tqdm_notebook
 from sklearn.model_selection import train_test_split
 
 import torch
+import random
 
 
 # -------------------- Data Utils --------------------
@@ -149,6 +150,18 @@ def predict(net, *arrays, **kwargs):
         
     return torch.cat(outputs)
     
+    
+def seed_all(seed=None):
+    """Set seed for random, numpy.random, torch and torch.cuda."""
+    random.seed(seed)
+    np.random.seed(seed)
+    if seed is None:
+        torch.manual_seed(np.random.randint(1e6))
+        torch.cuda.manual_seed(np.random.randint(1e6))
+    else:
+        torch.manual_seed(seed)
+        torch.cuda.manual_seed(seed)
+        
     
 # -------------------- Logging Utils --------------------
 
